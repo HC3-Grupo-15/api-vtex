5e2b38433b4179057fc99ee108606a0d21eb528d
Igor Sato <igoryouiti@hotmail.com>
2021-08-30 17:57:09 -0300

    continuacao da feature updateLeads

--- node/clients/leads.ts
+++ node/clients/leads.ts
@@ -1,14 +1,17 @@
 import type { InstanceOptions, IOContext, IOResponse } from '@vtex/api'
 import { ExternalClient } from '@vtex/api'
 
-
 export default class Leads extends ExternalClient {
   private routes = {
     leads: '/leads',
   }
 
   constructor(context: IOContext, options?: InstanceOptions) {
-    super('https://cbrdqaqq92.execute-api.sa-east-1.amazonaws.com', context, options)
+    super(
+      'https://cbrdqaqq92.execute-api.sa-east-1.amazonaws.com',
+      context,
+      options
+    )
   }
 
   public async getLeads(): Promise<string> {
@@ -20,6 +23,12 @@ export default class Leads extends ExternalClient {
   public async postLead(lead: Lead): Promise<string> {
     return this.http.post(this.routes.leads, lead, {
       metric: 'lead-post',
+    })
+  }
+
+  public async putLead(userId: string): Promise<string> {
+    return this.http.put(`${this.routes.leads}/${userId}`, {
+      metric: 'lead-put',
     })
   }
 
--- node/middlewares/updateLead.ts
+++ node/middlewares/updateLead.ts
@@ -2,22 +2,36 @@ import { json } from 'co-body'
 
 export async function updateLead(ctx: Context, next: () => Promise<any>) {
   const {
-    clients: { order: ordeClient, leads: leadsClient }
+    clients: { order: orderClient, leads: leadsClient },
   } = ctx
 
-  const body = (await json(ctx.req))
+  const body = await json(ctx.req)
   const orderId = body.OrderId
-  const orderData = (await ordeClient.getOrder(ctx, orderId))
+  const orderData = await orderClient.getOrder(ctx, orderId)
+
   ctx.status = 200
-
   const userId = orderData.clientProfileData.userProfileId
-  const userEmail = (await ordeClient.getEmail(ctx, userId))
 
-  const response = await leadsClient.getLeads().catch((reason) => { return reason?.response?.data })
+  const userEmail = await orderClient.getEmail(ctx, userId)
+  const { email } = userEmail[0]
+
+  const response = await leadsClient.getLeads().catch((reason) => {
+    return reason?.response?.data
+  })
+
   const allLeads = response.Items
 
-  const user = allLeads.find((item: any) => item.email === userEmail[0].email)
-  console.log(user)
+  const user = allLeads.find((item: any) => {
+    return item.email === email
+  })
+
+  if (!user) {
+    ctx.body = { msg: 'cliente não era lead' }
+  } else {
+    await leadsClient.putLead(user.id).catch((reason) => {
+      return reason?.response?.data
+    })
+  }
 
   await next()
 }
